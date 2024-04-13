7161f31511919533912e7a80d40440a280820165
Turbobot <turbobot@vercel.com>
2024-04-13 03:09:28 -0600

    feat(create-turbo): create https://github.com/t3-oss/create-t3-turbo

---
+++ packages/db/src/index.ts
@@ -0,0 +1,17 @@
+import { Client } from "@planetscale/database";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+
+import { connectionStr } from "./config";
+import * as auth from "./schema/auth";
+import * as post from "./schema/post";
+
+export const schema = { ...auth, ...post };
+
+export { mySqlTable as tableCreator } from "./schema/_table";
+
+export * from "drizzle-orm/sql";
+export { alias } from "drizzle-orm/mysql-core";
+
+const psClient = new Client({ url: connectionStr.href });
+
+export const db = drizzle(psClient, { schema });
